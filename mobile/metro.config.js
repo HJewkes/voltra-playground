63e5d6e4bd771c33442c789b2d5d999e97534e69
Henry Jewkes <hjewkes@Henrys-MBP.localdomain>
2026-01-22 15:45:03 -0700

    chore: switch to published @voltras/node-sdk@0.1.2
    - Update dependency from local file to npm package
    - Remove metro.config.js local SDK workaround

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
@@ -10,15 +9,6 @@ config.resolver.unstable_conditionNames = [
   'browser',
   'require', 
   'react-native',
-];
-
-// Support local SDK development via file: dependency
-// Metro needs to watch and resolve the symlinked package
-const sdkPath = path.resolve(__dirname, '../../voltra-node-sdk');
-config.watchFolders = [sdkPath];
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(sdkPath, 'node_modules'),
 ];
 
 module.exports = withNativeWind(config, { input: "./global.css" });
